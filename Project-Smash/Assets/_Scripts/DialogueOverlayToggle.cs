599adf6e5e96b25059390d64548b07077c4d69b2
Chris Rickabaugh <ChrisRick98@gmail.com>
2019-01-14 03:00:25 -0500

    Dialogue/UI Overlay done

---
+++ Project-Smash/Assets/_Scripts/DialogueOverlayToggle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueOverlayToggle : MonoBehaviour
+{
+
+    public GameObject overlayToggle;
+   // public TextReader script;
+ 
+
+// Start is called before the first frame update
+void Start()
+    {
+        overlayToggle = GameObject.Find("DialogueOverlay");
+        overlayToggle.SetActive(false);
+       // GameObject temp = GameObject.Find("TextBox");
+       // script = temp.GetComponent<TextReader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            overlayToggle.SetActive(true);
+
+            //script.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            overlayToggle.SetActive(false);
+
+           // script.enabled = false;
+        }
+    }
+}
---
+++ Project-Smash/Assets/_Scripts/TextReader.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class TextReader : MonoBehaviour
+{
+
+    
+//Text Reader Variables
+public Text txt;
+    public string path = "Assets/Text_Files/TestText.txt";
+    public int counter = 0;
+
+    //Text Scroller and Delay Variables
+    private string currentText;
+    public float delay = 0.03f;
+   
+    //List of Dialogue Lines
+    ArrayList textLines = new ArrayList();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        txt = GetComponent<Text>();
+        ReadString();
+        //Letter by letter string reveal
+        StartCoroutine(showText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = currentText;
+        if(Input.GetKeyDown(KeyCode.Space) && counter < textLines.Count-1)
+            //&& currentText == textLines[counter].ToString())
+        {           
+            counter++;
+            StartCoroutine(showText());
+        }
+
+    }
+    
+    //Reads input file for dialogue box.
+    void ReadString()
+    {
+        string temp;
+        //Read the text directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        while ((temp = reader.ReadLine()) != null)
+        {
+            textLines.Add(temp);
+        }
+        reader.Close();
+    }
+    
+    IEnumerator showText()
+    {
+            for(int j = 0; j < textLines[counter].ToString().Length; j++)
+            {
+                /*NOT DONE D: ---- ATTEMPTING TO ADD SPECIAL PAUSE CHARACTER FOR TEXT SCROLLING
+                if(textLines[counter].ToString()[j] == '#')
+                {
+                    textLines[counter].ToString().Remove(textLines[counter].ToString()[j],1);
+                    yield return new WaitForSeconds(0.6f);
+                }
+                */
+                //Type-Writer Effect Text
+                currentText = textLines[counter].ToString().Substring(0, j);
+                yield return new WaitForSeconds(delay);
+            }
+        
+    }
+}
